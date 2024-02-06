ff8cf2411a50fdad3186e34255eaea0982e5c089
Hieu Pham <hieupham.code@gmail.com>
2024-02-06 20:02:57 +0700

    Refator code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,9 @@
-import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
-
-import { useColorScheme } from '@mui/material'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeIcon from '@mui/icons-material/DarkMode'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-
-function ModeSelect() {
-  const { mode, setMode } = useColorScheme()
-  const handleChange = (e) => {
-    setMode(e.target.value)
-  }
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-      <Select
-        labelId="label-select-dark-light-mode"
-        id="select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-        <MenuItem value="light">
-          <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-            <LightModeIcon fontSize='small'/> Light
-          </div>
-        </MenuItem>
-        <MenuItem value="dark">
-          <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-            <DarkModeIcon fontSize='small'/> Dark
-          </div>
-        </MenuItem>
-        <MenuItem value="system">
-          <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-            <SettingsBrightnessIcon fontSize='small'/> System
-          </div>
-        </MenuItem>
-      </Select>
-    </FormControl>
-  )
-}
+import Board from './pages/Boards/_id'
 
 function App() {
   return (
-    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
-      <Box sx={{
-        backgroundColor: 'primary.light',
-        width: '100%',
-        height: (theme) => theme.trello.appBarHeight,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        <ModeSelect />
-      </Box>
-      <Box sx={{
-        backgroundColor: 'primary.dark',
-        width: '100%',
-        height: (theme) => theme.trello.boardBarHeight,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        Board bar
-      </Box>
-      <Box sx={{
-        width: '100%',
-        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-        backgroundColor: 'primary.main',
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        Board content
-      </Box>
-    </Container>
+    // React Router Dom
+    <Board />
   )
 }
 
---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,1 @@
+// Authencation signin sign up
