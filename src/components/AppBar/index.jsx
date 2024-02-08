7fbb8944b5cdbce636a1af949c85d125497a90e4
Hieu Pham <hieupham.code@gmail.com>
2024-02-08 20:44:51 +0700

    done board bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -15,6 +15,7 @@ import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
 import Tooltip from '@mui/material/Tooltip'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import Profiles from './Menus/Profiles'
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd'
 
 function AppBar() {
   return (
@@ -38,7 +39,7 @@ function AppBar() {
           <Recent />
           <Starred />
           <Templates />
-          <Button variant="outlined">Create</Button>
+          <Button variant="outlined" startIcon={<LibraryAddIcon />}>Create</Button>
         </Box>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,103 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
-    <Box sx={{
+    <Box px={2} sx={{
       width: '100%',
       height: (theme) => theme.trello.boardBarHeight,
-      backgroundColor: 'primary.dark',
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
-      Board bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label="HieuPham Developer"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label="Filters"
+          clickable
+        />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon/>}>Invite</Button>
+        <AvatarGroup
+          max={4}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title='HieuCode'>
+            <Avatar alt="Remy Sharp" src="https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-6/395895629_1735768630180082_6235557071848652993_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=LQqEAZSIVfcAX_CdL2m&_nc_ht=scontent.fhan3-4.fna&oh=00_AfCDLU-b_105IjC4GCaVeQRCGwEg0X3AJ2875ltYzttG0Q&oe=65C6908F" />
+          </Tooltip>
+          <Tooltip title='HieuCode'>
+            <Avatar alt="Remy Sharp" src="https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-6/395895629_1735768630180082_6235557071848652993_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=LQqEAZSIVfcAX_CdL2m&_nc_ht=scontent.fhan3-4.fna&oh=00_AfCDLU-b_105IjC4GCaVeQRCGwEg0X3AJ2875ltYzttG0Q&oe=65C6908F" />
+          </Tooltip>
+          <Tooltip title='HieuCode'>
+            <Avatar alt="Remy Sharp" src="https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-6/395895629_1735768630180082_6235557071848652993_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=LQqEAZSIVfcAX_CdL2m&_nc_ht=scontent.fhan3-4.fna&oh=00_AfCDLU-b_105IjC4GCaVeQRCGwEg0X3AJ2875ltYzttG0Q&oe=65C6908F" />
+          </Tooltip>
+          <Tooltip title='HieuCode'>
+            <Avatar alt="Remy Sharp" src="https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-6/395895629_1735768630180082_6235557071848652993_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=LQqEAZSIVfcAX_CdL2m&_nc_ht=scontent.fhan3-4.fna&oh=00_AfCDLU-b_105IjC4GCaVeQRCGwEg0X3AJ2875ltYzttG0Q&oe=65C6908F" />
+          </Tooltip>
+          <Tooltip title='HieuCode'>
+            <Avatar alt="Remy Sharp" src="https://scontent.fhan3-4.fna.fbcdn.net/v/t39.30808-6/395895629_1735768630180082_6235557071848652993_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=LQqEAZSIVfcAX_CdL2m&_nc_ht=scontent.fhan3-4.fna&oh=00_AfCDLU-b_105IjC4GCaVeQRCGwEg0X3AJ2875ltYzttG0Q&oe=65C6908F" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
